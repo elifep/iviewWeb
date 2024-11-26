73879bc11c5f688b7a65a122f708ad78ade19a1a
eep <elifep13@gmail.com>
2024-11-26 15:26:03 +0300

    güncel

--- src/App.jsx
+++ src/App.jsx
@@ -11,17 +11,18 @@ function App() {
             <Routes>
                 {/* Home route can lead to personal info or landing page */}
                 <Route path="/" element={<PersonalInfoFormPage />} />
-                    {/* Route for applying with a unique interview link */}
-                    <Route path="/apply/:uniqueId" element={<CandidateFlowPage />} />
+                {/* Route for applying with a unique interview link */}
+                <Route path="/apply/:uniqueId" element={<CandidateFlowPage />} />
 
+                {/* Route for personal info form */}
                 {/* Route for personal info form */}
                 <Route path="/personal-info" element={<PersonalInfoFormPage />} />
 
                 {/* Route for the candidate interview flow after the personal info form */}
                 <Route path="/interview" element={<InterviewFlowPage />} />
-                <Route path="/success-page" element={<SuccessPage />} />   
+                <Route path="/success-page" element={<SuccessPage />} />
 
-            
+
             </Routes>
         </Router>
     );
